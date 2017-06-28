eaff6f4939eda1d7701c8d18459d293e25f52a91
JaBorst <mai13ivw@studserv.uni-leipzig.de>
2017-06-28 17:15:52 +0200

    Revert "Timeline v2"

--- app/backend/flasky.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export FLASK_APP=server.py
-python -m flask run --host=0.0.0.0 
--- app/test.py
+++
@@ -1,2 +0,0 @@
-import requests
-res = requests.post('http://0.0.0.0:5000/ptagcloudapi', s={"sql":"Select * from letters;"})#json={"mytext":"lalala"})
